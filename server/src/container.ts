8e96735d76274a583e4c6f9c84b311c5de3e238d
shawn-dev-5790 <shawn.dev.5790@gmail.com>
2023-03-03 17:51:24 +0900

    feat: :sparkles: add dotenv to server pakage

--- server/src/container.ts
+++ server/src/container.ts
@@ -2,10 +2,21 @@ import { createDatabaseEngine, Database } from './database'
 import { UserTable } from './adaptor/database/tables/UserTable'
 import { UserRepo } from './adaptor/database/repositories/UserRepo'
 import { UserUseCase } from './application/usecases/UserUseCase'
+import * as dotenv from 'dotenv'
 
+dotenv.config()
+
+console.log(process.env)
 export class Container {
   // config
-  config = { localDB: { name: 'gg', user: 'root', pwd: '', host: '127.0.0.1' } }
+  config = {
+    localDB: {
+      name: process.env.LOCAL_DB_NAME,
+      user: process.env.LOCAL_DB_USER,
+      pwd: process.env.LOCAL_DB_PWD,
+      host: process.env.LOCAL_DB_HOST,
+    },
+  }
 
   // database
   localDB = new Database(createDatabaseEngine(this.config.localDB), [UserTable])
